e421ec99341ebae59d957f4c99af594f6517884f
Marek Belski <marek.belski@gmail.com>
2025-06-15 20:37:27 -0700

    Mines that blow up stay removed.

--- scenes/prototype/level/prototype_level.gd
+++ scenes/prototype/level/prototype_level.gd
@@ -2,5 +2,22 @@ extends Node3D
 
 signal level_won
 
+var level_state : LevelState
+
 func _on_exit_area_3d_player_exited():
 	level_won.emit()
+
+func _on_mine_exploded(node: Node3D):
+	level_state.mines_removed.append(node.name)
+
+func open_tutorials() -> void:
+	level_state.tutorial_read = true
+
+func _ready() -> void:
+	level_state = GameState.get_level_state(scene_file_path)
+	var mines := get_tree().get_nodes_in_group(&"mine")
+	for mine in mines:
+		if mine.name in level_state.mines_removed:
+			mine.queue_free()
+		else:
+			mine.exploded.connect(_on_mine_exploded.bind(mine))
