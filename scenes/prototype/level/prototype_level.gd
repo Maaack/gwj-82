175116726cfdcb49a3bff52b7751b4d2315e68e5
Marek Belski <marek.belski@gmail.com>
2025-06-16 23:38:28 -0700

    Fixes mines removed getting remembered.

--- scenes/prototype/level/prototype_level.gd
+++ scenes/prototype/level/prototype_level.gd
@@ -3,12 +3,14 @@ extends Node3D
 signal level_won
 
 var level_state : LevelState
+var mine_position_map : Dictionary[Node3D, Vector3]
 
 func _on_exit_area_3d_player_exited():
 	level_won.emit()
 
 func _on_mine_exploded(node: Node3D):
-	level_state.mines_removed.append(node.name)
+	var _original_position = mine_position_map[node]
+	level_state.mines_removed.append(_original_position)
 
 func open_tutorials() -> void:
 	level_state.tutorial_read = true
@@ -17,7 +19,8 @@ func _ready() -> void:
 	level_state = GameState.get_level_state(scene_file_path)
 	var mines := get_tree().get_nodes_in_group(&"mine")
 	for mine in mines:
-		if mine.name in level_state.mines_removed:
+		mine_position_map[mine as Node3D] = mine.global_position
+		if mine.global_position in level_state.mines_removed:
 			mine.queue_free()
 		else:
 			mine.exploded.connect(_on_mine_exploded.bind(mine))
--- scripts/level_state.gd
+++ scripts/level_state.gd
@@ -2,5 +2,5 @@ class_name LevelState
 extends Resource
 
 @export var level_tiles : Array[PackedScene]
-@export var mines_removed : Array[StringName]
+@export var mines_removed : Array[Vector3]
 @export var tutorial_read : bool = false
