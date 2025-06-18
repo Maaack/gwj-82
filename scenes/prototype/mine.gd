5df8fe1763376cfa72f3f0cd040d22456f5bc06e
parallax.rat <syntheticdream57@gmail.com>
2025-06-18 13:52:04 -0400

    Updates mine scene to include SFX, and modifies tween timing
    - Tween now jumps up 1.25 height instead of 2.0, and takes 0.6 seconds to complete instead of 0.2 to closer match the timing of the SFX.
    
    - Tween transition type set to BACK

--- scenes/prototype/mine.gd
+++ scenes/prototype/mine.gd
@@ -2,19 +2,21 @@ extends Area3D
 
 signal exploded
 
+@export var sfx: AudioStreamPlayer3D
+
 # Used to reference the player when triggered
 var player: Node3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		player = body
+		sfx.play()
 		var tween = get_tree().create_tween()
-		tween.tween_property(self,"position:y",2.0,0.2)
+		tween.tween_property(self,"position:y",1.25,0.6).set_trans(Tween.TRANS_BACK)
 		await tween.finished
 		player.immobile = true
 		explode()
 
 func explode() -> void:
-	##TODO Play SFX
 	print(name + " detonated at " + str(position))
 	exploded.emit()
