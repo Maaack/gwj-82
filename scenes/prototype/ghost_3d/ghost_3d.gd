261e152e97d4386b2e8c6566a4c4745aa0b6fcd4
Marek Belski <marek.belski@gmail.com>
2025-06-20 13:11:42 -0700

    I freaking hate rotations in 3D.

--- scenes/prototype/ghost_3d/ghost_3d.gd
+++ scenes/prototype/ghost_3d/ghost_3d.gd
@@ -20,7 +20,12 @@ func _on_track_refresh_timer_timeout() -> void:
 
 func _process(delta: float) -> void:
 	if global_position.distance_squared_to(target_position) > minimum_distance_squared:
-		var move_direction = target_position - global_position 
+		var move_direction : Vector3 = target_position - global_position
+		move_direction.y = 0
+		var angle_to : float = Vector3.FORWARD.signed_angle_to(move_direction, Vector3.UP)
+		var _angle_diff : float = angle_difference(%Pivot.rotation.y, angle_to)
+		var rotate_amount = _angle_diff * delta
+		%Pivot.rotate_y(rotate_amount)
 		move_direction = move_direction.normalized()
 		velocity = move_speed * delta * move_direction
 		move_and_slide()
