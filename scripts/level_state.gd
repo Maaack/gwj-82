4dd7148439b497e27e8c3bac923191ce7f513747
Marek Belski <marek.belski@gmail.com>
2025-06-20 10:50:40 -0700

    Adds zone state and has zone builder use it instead.

--- scripts/level_state.gd
+++ scripts/level_state.gd
@@ -1,7 +1,16 @@
 class_name LevelState
 extends Resource
 
-@export var level_tiles : Array[PackedScene]
+@export var zones : Array[ZoneState]
 @export var mines_removed : Array[Vector3]
 @export var tutorial_read : bool = false
 @export var starting_ghosts : int = 1
+
+func get_zone_state(builder_name : StringName) -> ZoneState:
+	for zone in zones:
+		if zone.builder_name == builder_name:
+			return zone
+	var _new_zone = ZoneState.new()
+	_new_zone.builder_name = builder_name
+	zones.append(_new_zone)
+	return _new_zone
---
+++ scripts/zone_state.gd
@@ -0,0 +1,5 @@
+class_name ZoneState
+extends Resource
+
+@export var builder_name : StringName
+@export var zones : Array[PackedScene]
