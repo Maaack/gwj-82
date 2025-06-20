1aba99eb6b33252c0d8d074340c030330e6f205e
parallax.rat <syntheticdream57@gmail.com>
2025-06-20 15:22:07 -0400

    Adds working scanner
    - Left click to place/pick up the scanner
    - Will flash a light to indicate the threat level in proximity to the scanner:
    -- RED: Extreme danger, mine is within 0.5m
    -- ORANGE: Moderate danger, mine is within 2.0m
    -- YELLOW: Low danger, mine is within 4.0m
    -- GREEN: No danger, no mine within 4.0m

--- addons/fpc/scanner_controller.gd
+++ addons/fpc/scanner_controller.gd
@@ -4,11 +4,20 @@ signal scanner_placed
 signal scanner_picked_up
 
 @export var scanner_object: PackedScene
-@export var hand_scanner_mesh: Node3D
 @export var camera: Camera3D
 @export var placement_range: int
+@export var hand_scanner: Node3D
 
-var holding_scanner: bool = true
+var scanner
+var holding_scanner: bool = true:
+	set(state):
+		holding_scanner = state
+		if holding_scanner:
+			hand_scanner.show()
+		else:
+			hand_scanner.hide()
+	get:
+		return holding_scanner
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("scanner"):
@@ -29,19 +38,34 @@ func try_to_place_scanner() -> void:
 	var result = space_state.intersect_ray(query)
 
 	if result and result.has("position"):
-		pass
+		place_scanner(result.position)
 
+
+func place_scanner(_position: Vector3) -> void:
+	holding_scanner = false
+	
+	scanner = scanner_object.instantiate()
+	var minefield = get_tree().get_first_node_in_group("item_spawner")
+	minefield.add_child(scanner)
+	scanner.global_position = _position
 
 
 func try_to_pickup_scanner() -> void:
+	if holding_scanner:
+		return
 	var space_state = get_world_3d().direct_space_state
 	var from = camera.global_transform.origin
 	var to = from + camera.global_transform.basis.z * -10
 
 	var query = PhysicsRayQueryParameters3D.create(from, to)
 	query.collide_with_areas = true
-	query.collide_with_bodies = true
 	var result = space_state.intersect_ray(query)
 
 	if result and result.has("collider") and result.collider.is_in_group("scanner"):
-		print("Pick UP")
+		pickup_scanner()
+
+
+func pickup_scanner() -> void:
+	holding_scanner = true
+	
+	scanner.queue_free()
--- addons/fpc/scanner_object.gd
+++ addons/fpc/scanner_object.gd
@@ -4,36 +4,59 @@ enum WarningLevel{
 	LOW,
 	MODERATE,
 	SEVERE,
+	NONE,
 	}
 
 const SEVERE_COLOR: Color = Color.RED
 const MODERATE_COLOR: Color = Color.ORANGE
 const LOW_COLOR: Color = Color.YELLOW
+const NONE_COLOR: Color = Color.GREEN
 
-const SEVERE_DELAY: float = 0.2
-const MODERATE_DELAY: float = 1.5
-const LOW_DELAY: float = 3.0
+const SEVERE_DELAY: float = 0.3
+const MODERATE_DELAY: float = 1
+const LOW_DELAY: float = 1.5
 
 @export var sfx: AudioStreamPlayer3D
 @export var light: OmniLight3D
 @export var timer: Timer
 
-var low_distance_threshold: float = 8.0
-var moderate_distance_threshold: float = 5.0
-var severe_distance_threshold: float = 2.0
+var low_distance_threshold: float = 4.0
+var moderate_distance_threshold: float = 2.0
+var severe_distance_threshold: float = 0.5
 var closest_mine_distance: float
+var mines: Array
 
 func _ready() -> void:
-	var closest_mine = get_closest(get_overlapping_areas())
-	closest_mine_distance = global_position.distance_to(closest_mine.global_position)
+	call_deferred("init_scanner")
+
+
+func init_scanner() -> void:
+	var mines = get_tree().get_nodes_in_group("mine")
+	print("Mines: " + str(mines.size()))
+	var closest_mine = get_closest(mines)
+	print("Closest mine: " + str(closest_mine))
+	if closest_mine != null:
+		closest_mine_distance = global_position.distance_to(closest_mine.global_position)
 	
 	if closest_mine_distance <= severe_distance_threshold:
 		mine_warning(WarningLevel.SEVERE)
+	elif closest_mine_distance <= moderate_distance_threshold:
+		mine_warning(WarningLevel.MODERATE)
+	elif closest_mine_distance <= low_distance_threshold:
+		mine_warning(WarningLevel.LOW)
+	else:
+		mine_warning(WarningLevel.NONE)
+		return
 
-func get_closest(_mines_in_range: Array) -> Node3D:
+
+func _physics_process(delta: float) -> void:
+	mines = get_overlapping_areas()
+
+
+func get_closest(_mines: Array) -> Node3D:
 	var closest: Node3D = null
 	
-	for mine in _mines_in_range:
+	for mine in _mines:
 		if closest == null:
 			closest = mine
 			continue
@@ -52,7 +75,25 @@ func mine_warning(_severity: WarningLevel) -> void:
 		WarningLevel.SEVERE:
 			light.light_color = SEVERE_COLOR
 			timer.wait_time = SEVERE_DELAY
+			timer.start()
+		WarningLevel.MODERATE:
+			light.light_color = MODERATE_COLOR
+			timer.wait_time = MODERATE_DELAY
+			timer.start()
+		WarningLevel.LOW:
+			light.light_color = LOW_COLOR
+			timer.wait_time = LOW_DELAY
+			timer.start()
+		WarningLevel.NONE:
+			light.light_color = NONE_COLOR
+			light.show()
+			light.light_energy = 1.0
+			timer.stop()
 
 
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	##TODO play sfx 
+	light.show()
+	print("Mine Warning!")
+	await get_tree().create_timer(0.1).timeout
+	light.hide()
