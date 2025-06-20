07155535322cbd13df58bbb5abcdbac987369ab8
Marek Belski <marek.belski@gmail.com>
2025-06-19 21:12:24 -0700

    Updates mine with mesh and hides it until triggered.

--- scenes/prototype/mine.gd
+++ scenes/prototype/mine.gd
@@ -1,20 +1,33 @@
+@tool
 extends Area3D
 
 signal exploded
 
+@export_tool_button("Trigger") var trigger_action = trigger
+@export_tool_button("Reset") var reset_action = reset
+
 # Used to reference the player when triggered
 var player: Node3D
+
+func trigger() -> void:
+	%MineMesh.show()
+	var tween = get_tree().create_tween()
+	tween.set_trans(Tween.TRANS_BACK)
+	tween.tween_property(%MineMesh,"position:y",1.45,0.3)
+	await tween.finished
+	player.immobile = true
+	explode()
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		player = body
-		var tween = get_tree().create_tween()
-		tween.tween_property(self,"position:y",2.0,0.2)
-		await tween.finished
-		player.immobile = true
-		explode()
+		trigger()
 
 func explode() -> void:
 	##TODO Play SFX
 	print(name + " detonated at " + str(position))
 	exploded.emit()
+
+func reset() -> void:
+	%MineMesh.hide()
+	%MineMesh.position.y = -0.15
